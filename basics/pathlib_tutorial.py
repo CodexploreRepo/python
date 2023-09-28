15714690a4194f5f5bb2eca8512e5370d02473cb
CodeXplore <codexplore.channel@gmail.com>
2023-09-28 22:48:52 +0800

    Update pathlib_tutorial.py

--- basics/pathlib_tutorial.py
+++ basics/pathlib_tutorial.py
@@ -5,4 +5,10 @@ from pathlib import Path
 data_path = Path("__file__").resolve().parents[1]
 
 # convert Pathlib to String
-data_path = data_path.as_posix()
+data_path_str = data_path.as_posix()
+
+# to create a folder if it is not exist
+folder_path = Path('/this/is/the/new/folder')
+# parents=True -> will create if parent folder is not existed
+# exist_ok=True -> to ignore the warning if the parent folder is already existed
+folder_path.mkdir(parents=True, exist_ok=True)
